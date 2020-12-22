884748370e8aa6559300fd21e8baefcb451b36ac
Stephen Gladney <stephen.gladney@salesloft.com>
2020-12-22 11:19:10 -0500

    rm useHistory forcing update to url

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -1,7 +1,3 @@
-import React from "react"
-import { useHistory } from "react-router-dom"
-
 export function Dashboard() {
-  // useHistory().push("/dashboard")
   return <h1>Hi, I'm the dashboard.</h1>
 }
